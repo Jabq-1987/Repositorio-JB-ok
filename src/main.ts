f7d4433a3dd92661095955d29f58c490b4de808c
Jabq-1987 <jboada@estud.tesa.edu.ec>
2026-03-02 11:37:25 -0500

    instalación cámara

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import router from './router';
 import { createPinia } from 'pinia';
 import { IonicVue } from '@ionic/vue';
 
+// Above the createApp() line
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+defineCustomElements(window);
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
